3d43a5a2178fb2fdfa518e8c2d251bbe65d63843
satya <satya@satya-Inspiron-5520>
2018-08-16 15:09:20 +0530

    updated TextPair class

--- nyse/src/main/java/com/nyse/keyvalues/TextPair.java
+++ nyse/src/main/java/com/nyse/keyvalues/TextPair.java
@@ -7,17 +7,17 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
-public class TextPair implements WritableComparable<TextPair>{
+public class TextPair implements WritableComparable<TextPair> {
 
 	Text first;
 	Text second;
-	
-	
+
 	public TextPair(Text first, Text second) {
 		super();
 		this.first = first;
 		this.second = second;
 	}
+
 	public TextPair(String first, String second) {
 		super();
 		this.first = new Text(first);
@@ -26,24 +26,60 @@ public class TextPair implements WritableComparable<TextPair>{
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-		// TODO Auto-generated method stub
 		this.first.write(out);
 		this.second.write(out);
-		
+
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
-		// TODO Auto-generated method stub
 		this.first.readFields(in);
 		this.second.readFields(in);
-		
+
 	}
 
 	@Override
-	public int compareTo(TextPair arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(TextPair other) {
+		int cmp = this.first.compareTo(other.first);
+		if (cmp != 0)
+			return cmp;
+		return this.second.compareTo(other.second);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TextPair other = (TextPair) obj;
+		if (first == null) {
+			if (other.first != null)
+				return false;
+		} else if (!first.equals(other.first))
+			return false;
+		if (second == null) {
+			if (other.second != null)
+				return false;
+		} else if (!second.equals(other.second))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TextPair [first=" + first + ", second=" + second + "]";
 	}
 
 }
